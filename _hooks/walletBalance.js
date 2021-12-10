ab67fc2a46d69066bf2d707fcefa8175e962405c
0xMillionz <madokakaname99@protonmail.com>
2021-12-10 17:04:02 -0500

    wallet balance

--- _hooks/walletBalance.js
+++ _hooks/walletBalance.js
@@ -1,7 +1,7 @@
 import { useWallet } from "@solana/wallet-adapter-react";
 import { LAMPORTS_PER_SOL } from "@solana/web3.js";
 import * as anchor from "@project-serum/anchor";
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const BalanceContext = createContext(null);
 
@@ -11,4 +11,26 @@ const connection = new anchor.web3.Connection(rpcUrl);
 export default function useWalletBalance() {
 	const [balance, setBalance] = useContext(BalanceContext);
 	return [balance, setBalance];
+}
+
+export const WalletBalanceProvider = ({ children }) => {
+	const wallet = useWallet();
+	const [ balance, setBalance ] = useState(0);
+
+	useEffect(() => {
+		(async () => {
+			if (wallet?.publicKey) {
+				const balance = await connection.getBalance(wallet.publicKey);
+				setBalance(balance/LAMPORTS_PER_SOL);
+			}
+		})();
+	}, [wallet, connection])
+
+	return (
+		<BalanceContext.Provider
+			value={[ balance, setBalance ]}	
+		>
+			{children}
+		</BalanceContext.Provider>
+	)
 }
