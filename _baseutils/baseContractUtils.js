1cea179c49e5a2dc54881b73b01e52f03074a30d
indianeros58 <0xDEADBEAT@gmail.com>
2021-12-10 14:51:54 -0500

    cleaned some stuff

--- _baseutils/baseContractUtils.js
+++ _baseutils/baseContractUtils.js
@@ -1,47 +1,38 @@
 import * as anchor from "@project-serum/anchor";
+import {PublicKey} from "@solana/web3.js";
+import * as splToken from '@project-serum/anchor';
 
 export const PROGRAM_ID = new PublicKey(idl.metadata.address);
 
-const program = new Program(idl, programID, provider);
+const program = new Program(idl, PROGRAM_ID, provider);
+const programMultisigWallet = new PublicKey("2to8Y37AJAsCT7XFpSrvidTpSoF13L8pLM1pWoTgSprd");
 
-const [mintAccount, mintBump] = await PublicKey.findProgramAddress(
-[Buffer.from("initmint"), programID.toBuffer()],
-programID
-);
-
-const [protocolDataAccount, protocolDataBump] = await PublicKey.findProgramAddress(
-[Buffer.from("protocoldataaccount")],
-programID
-);
-
-const [protocolATA, protocolATABump] = await PublicKey.findProgramAddress(
-[Buffer.from("usertokenaccount"), programMultisigWallet.toBuffer()],
-programID
-);
-
-const [userVault, userVaultBump] = await PublicKey.findProgramAddress(
-[Buffer.from("uservault"), wallet.publicKey.toBuffer()],
-programID
-);
-
-const [userATA, userATABump] = await PublicKey.findProgramAddress(
-[Buffer.from("usertokenaccount"), wallet.publicKey.toBuffer()],
-programID
-);
-
-export const InitializeMint = async () => {
+export const InitializeMint = async (wallet) => {
+	const [mintAccount, mintBump] = await PublicKey.findProgramAddress(
+		[Buffer.from("initmint"), PROGRAM_ID.toBuffer()],
+		PROGRAM_ID
+	);
+		
 	await program.rpc.initMint(mintBump, {
 		accounts:{
 		mint: mintAccount,
 		payer: wallet.publicKey,
-		systemProgram: programID,
+		systemProgram: PROGRAM_ID,
 		tokenProgram: splToken.TOKEN_PROGRAM_ID,
 		rent: web3.SYSVAR_RENT_PUBKEY,
 		},
 	});
 }
 
-export const CreateProtocolATA = async () => {
+export const CreateProtocolATA = async (wallet) => {
+	const [mintAccount, mintBump] = await PublicKey.findProgramAddress(
+		[Buffer.from("initmint"), PROGRAM_ID.toBuffer()],
+		PROGRAM_ID
+	);
+	const [protocolATA, protocolATABump] = await PublicKey.findProgramAddress(
+		[Buffer.from("usertokenaccount"), programMultisigWallet.toBuffer()],
+		PROGRAM_ID
+	);
 	await program.rpc.initUserAta(
 		{
 			userBump: protocolATABump,
@@ -55,60 +46,91 @@ export const CreateProtocolATA = async () => {
 				rent: web3.SYSVAR_RENT_PUBKEY,
 				mint: mintAccount,
 				tokenProgram: splToken.TOKEN_PROGRAM_ID,
-				systemProgram: programID,
+				systemProgram: PROGRAM_ID,
 			},
 		}
 	);
 }
 
-export const InitializeProtocolData = async () => {
+export const InitializeProtocolData = async (wallet) => {
+	const [protocolDataAccount, protocolDataBump] = await PublicKey.findProgramAddress(
+		[Buffer.from("protocoldataaccount")],
+		PROGRAM_ID
+	);
 	await program.rpc.initProtocolData(
 		protocolDataBump,
 		{
 			accounts:{
 				protocolDataAccount: protocolDataAccount,
 				owner: wallet.publicKey,
-				systemProgram: programID,
+				systemProgram: PROGRAM_ID,
 				rent: web3.SYSVAR_RENT_PUBKEY
 			},
 		}
 	);
 }
 
-export const InitializeUserVault = async () => {
+export const InitializeUserVault = async (wallet) => {
+	const [userVault, userVaultBump] = await PublicKey.findProgramAddress(
+		[Buffer.from("uservault"), wallet.publicKey.toBuffer()],
+		PROGRAM_ID
+	);
 	program.rpc.initUserVault(userVaultBump,
 		{
 			accounts:{
 			userAccount: userVault,
 			payer: wallet.publicKey,
 			user: wallet.publicKey,
-			systemProgram: programID,
+			systemProgram: PROGRAM_ID,
 		},
 	});
 }
 
-export const CreateUserATA = async () => {
+export const CreateUserATA = async (wallet) => {
+	const [mintAccount, mintBump] = await PublicKey.findProgramAddress(
+		[Buffer.from("initmint"), PROGRAM_ID.toBuffer()],
+		PROGRAM_ID
+	);
+	const [userATA, userATABump] = await PublicKey.findProgramAddress(
+		[Buffer.from("usertokenaccount"), wallet.publicKey.toBuffer()],
+		PROGRAM_ID
+	);
 	await program.rpc.initUserAta({
 			userBump: userATABump,
 			mintBump: mintBump,
 		},
 		{
 			accounts:{
-				userAta: userATAAccount,
+				userAta: userATA,
 				payer: wallet.publicKey,
 				user: wallet.publicKey,
 				rent: web3.SYSVAR_RENT_PUBKEY,
 				mint: mintAccount,
 				tokenProgram: splToken.TOKEN_PROGRAM_ID,
-				systemProgram: programID,
+				systemProgram: PROGRAM_ID,
 			},
 		}
 	);
 }
 
-export const DepositAssetPrintBond = async (asset_amount) => {
+export const DepositAssetPrintBond = async (wallet, asset_amount) => {
 	// todo: calculate bond amount from pyth oracle.
 	const bond_amount = 100;
+
+
+	const [protocolATA, protocolATABump] = await PublicKey.findProgramAddress(
+		[Buffer.from("usertokenaccount"), programMultisigWallet.toBuffer()],
+		PROGRAM_ID
+	);
+	const [userVault, userVaultBump] = await PublicKey.findProgramAddress(
+		[Buffer.from("uservault"), wallet.publicKey.toBuffer()],
+		PROGRAM_ID
+	);
+	const [userATA, userATABump] = await PublicKey.findProgramAddress(
+		[Buffer.from("usertokenaccount"), wallet.publicKey.toBuffer()],
+		PROGRAM_ID
+	);
+
 	await program.rpc.depositAsset(userBump, new anchor.BN(asset_amount), new anchor.BN(bond_amount), {
 		accounts:{
 		userAta: userATA,
@@ -120,7 +142,19 @@ export const DepositAssetPrintBond = async (asset_amount) => {
 	})
 }
 
-export const RedeemBonts = async () => {
+export const RedeemBonts = async (wallet) => {
+	const [mintAccount, mintBump] = await PublicKey.findProgramAddress(
+		[Buffer.from("initmint"), PROGRAM_ID.toBuffer()],
+		PROGRAM_ID
+	);
+	const [userVault, userVaultBump] = await PublicKey.findProgramAddress(
+		[Buffer.from("uservault"), wallet.publicKey.toBuffer()],
+		PROGRAM_ID
+	);
+	const [userATA, userATABump] = await PublicKey.findProgramAddress(
+		[Buffer.from("usertokenaccount"), wallet.publicKey.toBuffer()],
+		PROGRAM_ID
+	);
 	await program.rpc.redeemBonds({
 		userVaultBump: userVaultBump,
 		userAtaBump: userATABump,
@@ -133,13 +167,30 @@ export const RedeemBonts = async () => {
 		mintAuth: wallet.publicKey,
 		userAta: userATA,
 		tokenProgram: splToken.TOKEN_PROGRAM_ID,
-		systemProgram: programID,
+		systemProgram: PROGRAM_ID,
 		},
 	});
 }
 
 // stake amount is in twst
-export const Stake = async(amount) => {
+export const Stake = async(wallet, amount) => {
+	const [protocolDataAccount, protocolDataBump] = await PublicKey.findProgramAddress(
+		[Buffer.from("protocoldataaccount")],
+		PROGRAM_ID
+	);
+	const [protocolATA, protocolATABump] = await PublicKey.findProgramAddress(
+		[Buffer.from("usertokenaccount"), programMultisigWallet.toBuffer()],
+		PROGRAM_ID
+	);
+	const [userVault, userVaultBump] = await PublicKey.findProgramAddress(
+		[Buffer.from("uservault"), wallet.publicKey.toBuffer()],
+		PROGRAM_ID
+	);
+	const [userATA, userATABump] = await PublicKey.findProgramAddress(
+		[Buffer.from("usertokenaccount"), wallet.publicKey.toBuffer()],
+		PROGRAM_ID
+	);
+
 	await program.rpc.stake({
 		userVault: userVaultBump,
 		userAta: userATABump,
@@ -159,7 +210,24 @@ export const Stake = async(amount) => {
 }
 
 // unstake amount is stwst
-export const Unstake = async () => {
+export const Unstake = async (wallet, amount) => {
+
+	const [protocolDataAccount, protocolDataBump] = await PublicKey.findProgramAddress(
+		[Buffer.from("protocoldataaccount")],
+		PROGRAM_ID
+	);
+	const [protocolATA, protocolATABump] = await PublicKey.findProgramAddress(
+		[Buffer.from("usertokenaccount"), programMultisigWallet.toBuffer()],
+		PROGRAM_ID
+	);
+	const [userVault, userVaultBump] = await PublicKey.findProgramAddress(
+		[Buffer.from("uservault"), wallet.publicKey.toBuffer()],
+		PROGRAM_ID
+	);
+	const [userATA, userATABump] = await PublicKey.findProgramAddress(
+		[Buffer.from("usertokenaccount"), wallet.publicKey.toBuffer()],
+		PROGRAM_ID
+	);
 	await program.rpc.unstake({
 		userVault: userVaultBump,
 		userAta: userATABump,
@@ -177,7 +245,19 @@ export const Unstake = async () => {
 	});
 }
 
-export const Rebase = async () => {
+export const Rebase = async (wallet) => {
+	const [mintAccount, mintBump] = await PublicKey.findProgramAddress(
+		[Buffer.from("initmint"), PROGRAM_ID.toBuffer()],
+		PROGRAM_ID
+	);
+	const [protocolDataAccount, protocolDataBump] = await PublicKey.findProgramAddress(
+		[Buffer.from("protocoldataaccount")],
+		PROGRAM_ID
+	);
+	const [protocolATA, protocolATABump] = await PublicKey.findProgramAddress(
+		[Buffer.from("usertokenaccount"), programMultisigWallet.toBuffer()],
+		PROGRAM_ID
+	);
 	await program.rpc.rebase({
 		mintBump: mintBump,
 		protocolDataBump: protocolDataBump,
@@ -188,12 +268,17 @@ export const Rebase = async () => {
 		mint: mintAccount,
 		owner: wallet.publicKey,
 		tokenProgram: splToken.TOKEN_PROGRAM_ID,
-		systemProgram: programID,
+		systemProgram: PROGRAM_ID,
 		}
 	});
 }
 
-export const ChangeRebaseRate = async (new_rate) => {
+export const ChangeRebaseRate = async (wallet, new_rate) => {
+	const [protocolDataAccount, protocolDataBump] = await PublicKey.findProgramAddress(
+		[Buffer.from("protocoldataaccount")],
+		PROGRAM_ID
+	);
+
 	await program.rpc.changeRebaseRate(protocolDataBump, 
 		new anchor.BN(new_rate),
 		{
