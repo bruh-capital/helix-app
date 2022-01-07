6b76327dfbb3cec6e6de66f8f83fda2f8d50c5ba
0xMillionz <madokakaname99@protonmail.com>
2022-01-07 00:22:57 -0500

    style fixes

--- App/_includes/connection.js
+++ App/_includes/connection.js
@@ -14,17 +14,11 @@ export default function ConnectionButton(props) {
 	const [selected, setSelected] = useState(networks[0].value);
   
 	return (
-	  <div className="w-72 fixed top-16">
+	  <div className="w-72">
 		<Listbox value={selected} onChange={setSelected}>
 		  <div className="relative mt-1">
-			<Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
+			<Listbox.Button className="relative w-full py-6 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-purple-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
 			  <span className="block truncate">{selected.label}</span>
-			  <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-				<SelectorIcon
-				  className="w-5 h-5 text-gray-400"
-				  aria-hidden="true"
-				/>
-			  </span>
 			</Listbox.Button>
 			<Transition
 			  as={Fragment}
@@ -32,12 +26,12 @@ export default function ConnectionButton(props) {
 			  leaveFrom="opacity-100"
 			  leaveTo="opacity-0"
 			>
-			  <Listbox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+			  <Listbox.Options className="absolute z-50 w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
 				{networks.map((network, index) => (
 				  <Listbox.Option
 					key={index}
 					className={({ active }) =>
-					  `${active ? 'text-amber-900 bg-amber-100' : 'text-gray-900'}
+					  `${active ? 'text-purple-900 bg-purple-100' : 'text-gray-900'}
 							cursor-default select-none relative py-2 pl-10 pr-4`
 					}
 					value={network.label}
@@ -54,7 +48,7 @@ export default function ConnectionButton(props) {
 						{selected ? (
 						  <span
 							className={`${
-							  active ? 'text-amber-600' : 'text-amber-600'
+							  active ? 'text-purple-600' : 'text-purple-600'
 							}
 								  absolute inset-y-0 left-0 flex items-center pl-3`}
 						  >
