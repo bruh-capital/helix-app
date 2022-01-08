fd40780fe9511629c3855cb5afd71cf2746ba00b
0xMillionz <madokakaname99@protonmail.com>
2022-01-07 21:48:43 -0500

    bump shit

--- App/_baseutils/programInteractions.js
+++ App/_baseutils/programInteractions.js
@@ -14,15 +14,70 @@ export default class HelixInteractions {
 		this.provider = new anchor.Provider(this.connection, wallet, anchor.Provider.defaultOptions());
 		this.program = new anchor.Program(idl, this.PROGRAM_ID, provider);
 	}
-
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////////
 	// TOP LEVEL PROGRAM INTERACTIONS
-
 	/**
 	 * Stakes HLX tokens within the protocol to earn rebase rewards 
 	 * @param {BN} amount amount of HLX to stake
 	 */
 	StakeHLX = async (amount) => {
+		const [protocolDataAccount, protocolDataBump] = await PublicKey.findProgramAddress(
+			[Buffer.from("protocoldataaccount")],
+			this.PROGRAM_ID
+		);
+
+		const [protocolATA, protocolATABump] = await PublicKey.findProgramAddress(
+			[
+				Buffer.from("usertokenaccount"),
+				process.env.NEXT_PUBLIC_MULTISIG_PUBKEY
+			]
+		);
+
+		const [userVault, userVaultBump] = await PublicKey.findProgramAddress(
+			[
+				Buffer.from("uservault"),
+				this.wallet.publicKey.toBuffer()
+			],
+			this.PROGRAM_ID
+		);
+
+		const [userATA, userATABump] = await PublicKey.findProgramAddress(
+			[
+				Buffer.from("usertokenaccount"),
+				this.wallet.publicKey.toBuffer()
+			],
+			this.PROGRAM_ID
+		);
+
+		await this.program.rpc.stake(
+			{
+				userVaultBump: userVaultBump,
+				useAta: userATABump,
+				protocolData: protocolDataBump,
+				protocolATABump: protocolATABump,
+
+			},
+			amount,
+			{
+				accounts: {
+					userAta: userATA,
+					protocAta: protocolATA,
+					userVault: userVault,
+					protocolData: PrococolDataAccount,
+					user: this.wallet.publicKey,
+					tokenProgram: this.PROGRAM_ID,
+				},
+				signers: [this.wallet.Keypair]
+			}
+		);
+	};
+
+	/**
+	 * Unstakes available HLX tokens the user has staked within the protocol
+	 * @param {BN} amount
+	 */
+	UnstakeHLX = async (amount) => {
 		const [protocolDataAccount, protocolDataBump] = await PublicKey.findProgramAddress(
 			[Buffer.from("protocoldataaccount")],
 			this.PROGRAM_ID
@@ -51,32 +106,11 @@ export default class HelixInteractions {
 			this.PROGRAM_ID
 		);
 
-		await this.program.rpc.stake(
+		await this.program.rpc.unstake(
 			{
-				userVaultBump: userVaultBump,
-				useAta: userATABump,
-				protocolData: protocolDataBump,
-			},
-			new anchor.BN(amount),
-			{
-				accounts: {
-					userAta: userATA,
-					protocAta: protocolATA,
-					userVault: userVault,
-					protocolData: PrococolDataAccount,
-					user: this.wallet.publicKey,
-					tokenProgram: this.PROGRAM_ID,
-				},
-				signers: [this.wallet.Keypair]
-			}
-		);
-	};
-	/**
-	 * Unstakes available HLX tokens the user has staked within the protocol
-	 * @param {BN} amount
-	 */
-	UnstakeHLX = async (amount) => {
 
+			}
+		)
 	};
 
 	/**
