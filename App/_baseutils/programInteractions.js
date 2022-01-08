aafb444dac859631fb964174fe850e009b05fce6
0xMillionz <madokakaname99@protonmail.com>
2022-01-07 19:15:25 -0500

    stake hook

--- App/_baseutils/programInteractions.js
+++ App/_baseutils/programInteractions.js
@@ -1,6 +1,7 @@
 import * as anchor from '@project-serum/anchor';
 import { SystemProgram } from '@solana/web3.js'
 import * as idl from '@idl/twist.json';
+import AppMetaTagComponent from '@includes/metaTags';
 
 // TODO(@MILLIONZ): implement error codes within the program so we can show proper errors to the user
 export default class HelixInteractions {
@@ -33,12 +34,49 @@ export default class HelixInteractions {
 				process.env.NEXT_PUBLIC_MULTISIG_PUBKEY
 			]
 		);
+
+		const [userVault, userVaultBump] = await PublicKey.findProgramAddress(
+			[
+				Buffer.from("uservault"),
+				this.wallet.publicKey.toBuffer()
+			],
+			this.PROGRAM_ID
+		);
+
+		const [userATA, userATABump] = await PublicKey.findProgramAddress(
+			[
+				Buffer.from("usertokenaccount"),
+				this.wallet.publicKey.toBuffer()
+			],
+			this.PROGRAM_ID
+		);
+
+		await this.program.rpc.stake(
+			{
+				userVaultBump: userVaultBump,
+				useAta: userATABump,
+				protocolData: protocolDataBump,
+			},
+			new anchor.BN(amount),
+			{
+				accounts: {
+					userAta: userATA,
+					protocAta: protocolATA,
+					userVault: userVault,
+					protocolData: PrococolDataAccount,
+					user: this.wallet.publicKey,
+					tokenProgram: this.PROGRAM_ID,
+				},
+				signers: [this.wallet.Keypair]
+			}
+		);
 	};
 	/**
 	 * Unstakes available HLX tokens the user has staked within the protocol
 	 * @param {BN} amount
 	 */
 	UnstakeHLX = async (amount) => {
+
 	};
 
 	/**
