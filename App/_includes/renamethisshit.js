422b31269f57b19ce98eb8b4fb0c0b930f018fe4
0xMillionz <madokakaname99@protonmail.com>
2022-01-11 14:14:16 -0500

    pls rename

--- App/_includes/renamethisshit.js
+++ App/_includes/renamethisshit.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+// Hello change names pls
 export default function dashboardUi(props) {
 	return(
 		<div className="grid grid-cols-2 grid-rows-2 sm:grid-cols-1 sm:grid-rows-4">
