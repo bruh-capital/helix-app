e680ecb3e90969e822139b9f6a0021c78a3615a9
0xMillionz <madokakaname99@protonmail.com>
2022-01-11 18:21:33 -0500

    maybe graph loading???

--- App/_includes/dashboardUi.js
+++ App/_includes/dashboardUi.js
@@ -8,10 +8,17 @@ export default function dashboardUi(props) {
 				console.log(protocol.label);
 				return(
 					<div className="card bg-white col-span-1 row-span-1">
+						<h3 className="text-md text-gray8">
+							{protocol.label}
+						</h3>
 						<div className="card-body">
-							<h3 className="text-md text-gray8">
-								{protocol.label}
-							</h3>
+							{protocol.values === undefined ?
+								(
+									<svg class="animate-spin h-5 w-5" viewBox="0 0 24 24"/>
+								) : (
+									<span>Graph Goes here lol</span>
+								)
+							}
 						</div>
 					</div>
 				);
