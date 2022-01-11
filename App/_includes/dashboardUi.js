3e13da8591a9867396e11723493ea1101a96860a
0xMillionz <madokakaname99@protonmail.com>
2022-01-11 18:16:47 -0500

    font colors + card (messed up one bigger than the other)

--- App/_includes/dashboardUi.js
+++ App/_includes/dashboardUi.js
@@ -2,15 +2,17 @@ import React from "react";
 
 export default function dashboardUi(props) {
 	return(
-		<div className="py-5 grid md:grid-cols-2 md:grid-rows-2 sm:grid-cols-1 sm:grid-rows-4">
+		<div className="py-5 grid md:grid-cols-2 space-x-2 space-y-2 md:grid-rows-2 sm:grid-cols-1 sm:grid-rows-4">
 		{
 			props.protocolData?.map(function (protocol, index) {
 				console.log(protocol.label);
 				return(
-					<div className="bg-white">
-						<span className="text-black font-bold text-3xl">
-							{protocol.label}
-						</span>
+					<div className="card bg-white col-span-1 row-span-1">
+						<div className="card-body">
+							<h3 className="text-md text-gray8">
+								{protocol.label}
+							</h3>
+						</div>
 					</div>
 				);
 			})
