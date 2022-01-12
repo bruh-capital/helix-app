9c10e0c007122bf8d8cfdf420d361927d07bb5ef
0xMillionz <madokakaname99@protonmail.com>
2022-01-12 12:44:56 -0500

    starting refactor of stake component

--- App/_includes/dashboardUi.js
+++ App/_includes/dashboardUi.js
@@ -18,8 +18,6 @@ export default function dashboardUi(props) {
 		<div className="py-5 grid md:grid-cols-2 space-x-2 space-y-2 md:grid-rows-2 sm:grid-cols-1 sm:grid-rows-4">
 		{
 			props.protocolData?.map(function (protocol, index) {
-				console.log(protocol.label);
-				console.log(typeof protocol.values);
 				return(
 					<div className="card bg-white col-span-1 row-span-1">
 						<h3 className="text-left text-md text-gray8 mt-5 ml-10">
@@ -30,10 +28,11 @@ export default function dashboardUi(props) {
 								(
 									<div className="bg-gray6 rounded-md w-full h-24 animate-pulse"></div>
 								) : (
-									<AreaChart data={}>
-
+									// TODO(millionz): make this functional lol
+									<AreaChart data={protocol.values}>
+										<XAxis dataKey="time" />
+										<YAxis dataKey={protocol.label} />
 									</AreaChart>	
-									//<span>Graph Goes here lol</span>
 								)
 							}
 						</div>
