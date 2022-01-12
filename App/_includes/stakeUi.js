d9002fdc2971872a14e2ded6f8f37ed8821650e8
0xMillionz <madokakaname99@protonmail.com>
2022-01-12 17:57:42 -0500

    update stake layout + wallet button

--- App/_includes/stakeUi.js
+++ App/_includes/stakeUi.js
@@ -16,23 +16,20 @@ export default function StakeInterface(props) {
 	} = HelixWrapper();
 
 	return(
-		<>
-			<div className="grid justify-center w-full space-x-5 md:grid-cols-2 sm:grid-cols-1">
-				<div className="card flex max-w-sm justify-center bg-white p-3 mt-10">
-					<h3 className="card-title">APY</h3>
-					<div className="divide"></div>
-					<span className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#58B9FF] to-[#FF61DB]">instert APY here</span>
-				</div>
-				<div className="card flex max-w-sm justify-center bg-white p-3 mt-10">
-					<h3 className="card-title">TVL</h3>
-					<div className="divide"></div>
-					<span className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#58B9FF] to-[#FF61DB]">instert APY here</span>
-				</div>
+		<div className="grid justify-items-center w-full space-x-5 md:grid-cols-2 sm:grid-cols-1">
+			<div className="card flex max-w-sm justify-center bg-white p-3 mt-10">
+				<h3 className="card-title">APY</h3>
+				<div className="divide"></div>
+				<span className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#58B9FF] to-[#FF61DB]">instert APY here</span>
 			</div>
-			<div className="card flex max-w-md justify-center bg-white p-3 mt-10">
+			<div className="card flex max-w-sm justify-center bg-white p-3 mt-10">
+				<h3 className="card-title">TVL</h3>
+				<div className="divide"></div>
+				<span className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#58B9FF] to-[#FF61DB]">instert APY here</span>
+			</div>
+			<div className="card flex max-w-md justify-center col-span-2 bg-white p-3 mt-10">
 				<h3 className="card-title">{operation}</h3>
-
 			</div>
-		</>
+		</div>
 	);
 }
