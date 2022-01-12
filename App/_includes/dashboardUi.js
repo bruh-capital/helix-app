35ce8798a1bd7efd455866887007ce5dc9839395
0xMillionz <madokakaname99@protonmail.com>
2022-01-12 18:35:35 -0500

    fixed scuffed spacing and a lot of other things

--- App/_includes/dashboardUi.js
+++ App/_includes/dashboardUi.js
@@ -15,11 +15,11 @@ import {
 
 export default function dashboardUi(props) {
 	return(
-		<div className="my-5 grid md:grid-cols-2 space-x-2 space-y-2 md:grid-rows-2 sm:grid-cols-1 sm:grid-rows-4">
+		<div className="my-5 grid md:grid-cols-2 md:grid-rows-2 sm:grid-cols-1 sm:grid-rows-4">
 		{
 			props.protocolData?.map(function (protocol, index) {
 				return(
-					<div className="card bg-white col-span-1 row-span-1 mt-2 ml-2">
+					<div className="card bg-white col-span-1 row-span-1 m-2">
 						<h3 className="text-left text-md text-gray8 mt-5 ml-10">
 							{protocol.label}
 						</h3>
