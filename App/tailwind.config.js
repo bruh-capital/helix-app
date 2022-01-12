b0ee4459d765454abd5ff55b5c1d4d4cb080a20a
0xMillionz <madokakaname99@protonmail.com>
2022-01-12 17:32:42 -0500

    bro why is purge eating all my css on prod (fixed???)

--- App/tailwind.config.js
+++ App/tailwind.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  purge: [
-    './src/components/**/*.{js,ts,jsx,tsx}', './src/pages/**/*.{js,ts,jsx,tsx}'
-  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
