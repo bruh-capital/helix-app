2a4f6b6245a2d597dc484b15d23d9c34793799c8
0xMillionz <madokakaname99@protonmail.com>
2022-01-12 15:54:58 -0500

    mismatched sizes fixed

--- App/_includes/dashboardUi.js
+++ App/_includes/dashboardUi.js
@@ -15,18 +15,18 @@ import {
 
 export default function dashboardUi(props) {
 	return(
-		<div className="py-5 grid md:grid-cols-2 space-x-2 space-y-2 md:grid-rows-2 sm:grid-cols-1 sm:grid-rows-4">
+		<div className="my-5 grid md:grid-cols-2 space-x-2 space-y-2 md:grid-rows-2 sm:grid-cols-1 sm:grid-rows-4">
 		{
 			props.protocolData?.map(function (protocol, index) {
 				return(
-					<div className="card bg-white col-span-1 row-span-1">
+					<div className="card bg-white col-span-1 row-span-1 mt-2 ml-2">
 						<h3 className="text-left text-md text-gray8 mt-5 ml-10">
 							{protocol.label}
 						</h3>
 						<div className="card-body">
 							{typeof protocol.values === "undefined" ?
 								(
-									<div className="bg-gray6 rounded-md w-full h-24 animate-pulse"></div>
+									<div className="bg-gray6 rounded-md w-full h-24 animate-pulse" />
 								) : (
 									// TODO(millionz): make this functional lol
 									<AreaChart data={protocol.values}>
