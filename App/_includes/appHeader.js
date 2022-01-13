a8b76395a5c7b44398ee9dbf9528aa0f976c032b
0xMillionz <madokakaname99@protonmail.com>
2022-01-13 13:52:06 -0500

    added shimmer effect to loading icon

--- App/_includes/appHeader.js
+++ App/_includes/appHeader.js
@@ -11,6 +11,24 @@ export default function Header(props) {
 	const {theme, setTheme} = useContext(ThemeContext);
 	const {page, setPage} = useContext(PageContext);
 
+	const toBase64 = (str) => typeof window === 'undefined'
+		? Buffer.from(str).toString('base64')
+		: window.btoa(str)
+
+	const shimmer = (w, h) => `
+		<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+			<defs>
+				<linearGradient id="g">
+					<stop stop-color="#333" offset="20%" />
+					<stop stop-color="#222" offset="50%" />
+					<stop stop-color="#333" offset="70%" />
+				</linearGradient>
+			</defs>
+			<rect width="${w}" height="${h}" fill="#333" />
+			<rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+			<animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+		</svg>`;
+
 	return(
 		<Popover className='dappHeader'>
 		{({ open }) => (
@@ -26,6 +44,8 @@ export default function Header(props) {
 								alt="Helix DAO header icon"
 								width="72"
 								height="72"
+								placeholder="blur"
+								blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(62, 62))}`}
 							/>
 						</div>
 					</div>
