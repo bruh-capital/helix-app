5229c1a305f4fb8b2c3321bd2e24e2d38be43a46
0xMillionz <madokakaname99@protonmail.com>
2022-01-21 16:17:48 -0500

    revert

--- App/_layouts/baseDappLayout.js
+++ App/_layouts/baseDappLayout.js
@@ -2,18 +2,19 @@ import React, {useState} from 'react';
 import Head from 'next/head';
 import DappHeader from '@includes/appHeader';
 
+// NOTE: don't change these to div or shit will break
 export default function BaseDappLayout(props) {
 	return(
-		<div className='overflow-hidden'>
+		<main className='overflow-hidden'>
 			<Head>
 				<title>{props.title}</title>
 			</Head>
-			<div className='overflow-hidden bg-hero-gradient bg-cover'>
+			<body className='overflow-hidden bg-hero-gradient bg-cover'>
 				<DappHeader/>
 				<div className="relative text-center text-white content-center justify-center max-w-5xl px-10 sm:px-3 mx-auto pb-14 lg:pb-20 xl:pb-28">
 					{props.children}
 				</div>
-			</div>
-		</div>
+			</body>
+		</main>
 	);
 }
