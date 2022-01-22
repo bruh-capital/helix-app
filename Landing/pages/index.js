c3d74d90643447684d1b8b9d9511ff62b18d6515
0xMillionz <madokakaname99@protonmail.com>
2022-01-22 18:22:01 -0500

    fixing up landing

--- Landing/pages/index.js
+++ Landing/pages/index.js
@@ -1,10 +1,17 @@
-import { Head } from "next/document";
+import Link from "next/link";
 import MetaTagComponent from "@includes/metatags";
 
 export default function Home() {
   return (
     <MetaTagComponent>
-      <div className="hero min-h-screen" style={{backgroundImage: "url(/helixbanner.png)"}} />
+      <div className="hero min-h-screen h-full" style={{backgroundImage: "url(/pageassets/narrow_banner.png)"}}>
+        <Link href="https://app.helixdao.org">
+          <button
+            style={{background: "linear-gradient(to right, #58B9FF, #FF61DB)"}}
+            className="translate-y-28 text-xl font-bold text-white py-3 px-6 rounded-md "
+          >Launch App</button>
+        </Link>
+      </div>
     </MetaTagComponent>
   )
 }
