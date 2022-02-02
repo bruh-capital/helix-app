3ddbef16a403137e6b870268f2a7e45bbb3d20a8
0xMillionz <madokakaname99@protonmail.com>
2022-02-02 00:24:38 -0500

    fixing hero image

--- Landing/_includes/hero.js
+++ Landing/_includes/hero.js
@@ -1,6 +1,6 @@
 export default function Hero(props) {
 	return(
-		<div className="h-screen bg-token-image">
+		<div className="h-screen flex bg-tokenbg -translate-y-32 -mb-32">
 			{/*
 			<video className="z-20 -translate-y-32" autoPlay loop>
 				<source src="/landingassets/landingpage/bg/k.mp4" type="video/mp4"/>
