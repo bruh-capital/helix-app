4cee0b5e1d733980462537df61bdd6a0c677a631
0xMillionz <madokakaname99@protonmail.com>
2022-02-02 00:20:37 -0500

    header cleanups

--- Landing/_includes/hero.js
+++ Landing/_includes/hero.js
@@ -1,9 +1,11 @@
 export default function Hero(props) {
 	return(
-		<div className="h-screen">
-			<video className="z-0 -translate-y-32" autoPlay loop>
+		<div className="h-screen bg-token-image">
+			{/*
+			<video className="z-20 -translate-y-32" autoPlay loop>
 				<source src="/landingassets/landingpage/bg/k.mp4" type="video/mp4"/>
 			</video>
+			*/}
 		</div>
 	);
 }
