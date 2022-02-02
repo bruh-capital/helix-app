4153e3bfa7601ff1c431fd285f1e5ecd8ab6a7b9
0xMillionz <madokakaname99@protonmail.com>
2022-02-02 17:40:11 -0500

    hero balling (some weird spacing)

--- Landing/_includes/hero.js
+++ Landing/_includes/hero.js
@@ -11,9 +11,12 @@ export default function Hero(props) {
 					Get more out of your tokens
 				</span>
 				<span className="text-xl font-bold text-white mb-10">HelixDAO seeks to build user owned platforms to benefit token holders</span>
-				<div className="flex m-auto flex-row space-x-8">
-					<button className="bg-black text-white text-xl px-6 py-3 font-semibold border-solid border-4 border-blue-400 rounded-xl">Launch App</button>
-					<button className="bg-black text-white text-xl px-6 py-3 font-semibold border-solid border-4 border-purple-400 rounded-xl">Marketplace</button>
+				<div className="flex m-auto flex-row space-x-12">
+					<button className="bg-black text-white text-xl px-8 py-4 font-semibold border-solid border-4 border-[#59A6F4] rounded-2xl shadow-blue-glow-md hover:shadow-blue-glow-lg hover:scale-105">Launch App</button>
+					<button className="bg-black text-white text-xl px-8 py-4 font-semibold border-solid border-4 border-[#E66EE9] rounded-2xl shadow-pink-glow-md hover:shadow-pink-glow-lg hover:scale-105">
+						Marketplace
+						<span className="absolute inline-flex items-center justify-center px-3 py-2 text-sm font-bold leading-none text-white bg-[#E66EE9] rounded-full -translate-y-7 translate-x-1">WIP</span>	
+					</button>
 				</div>
 			</div>
 		</div>
--- Landing/tailwind.config.js
+++ Landing/tailwind.config.js
@@ -11,6 +11,12 @@ module.exports = {
       backgroundImage: {
         "tokenbg": "url('/landingassets/landingpage/bg/tokens.png')",
       },
+      boxShadow: {
+        'pink-glow-md': '0px 0px 20px #E66EE9',
+        'pink-glow-lg': '0px 0px 40px #E66EE9',
+        'blue-glow-md': '0px 0px 20px #59A6F4',
+        'blue-glow-lg': '0px 0px 40px #59A6F4',
+      },
     },
   },
 }
