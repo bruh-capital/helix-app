cc5a7fa7005a64c47fa1447d5e73fc9ea16a5242
0xMillionz <madokakaname99@protonmail.com>
2022-02-02 00:06:15 -0500

    assets

---
+++ Landing/_includes/hero.js
@@ -0,0 +1,9 @@
+export default function Hero(props) {
+	return(
+		<div className="h-screen">
+			<video className="z-0 -translate-y-32" autoPlay loop>
+				<source src="/landingassets/landingpage/bg/k.mp4" type="video/mp4"/>
+			</video>
+		</div>
+	);
+}
