9d69ee8e33ca6fb6db476f2227a6199bcbb25c90
0xMillionz <madokakaname99@protonmail.com>
2022-02-07 13:34:44 -0500

    tailwind purge fixes

--- apps/Multisig/tailwind.config.js
+++ apps/Multisig/tailwind.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  purge: false,
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./_hooks/**/*.{js,ts,jsx,tsx}",
+    "./_includes/**/*.{js,ts,jsx,tsx}",
+    "./_layouts/**/*.{js,ts,jsx,tsx}",
+    "./_context/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
