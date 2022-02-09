74a905a1fa638f57a748b9f02aafdf37a503b672
0xMillionz <madokakaname99@protonmail.com>
2022-02-09 00:24:43 -0500

    blow up everything yay

--- apps/Dapp/_context/helixClientContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-import HelixWrapper from "helix-client";
-
-const clientContext = createContext({
-	  	client: new HelixWrapper(),
-		setClient: ()=>{}
-});
-
-export default clientContext;
