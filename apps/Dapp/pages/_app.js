af2e6b9dcc55b3204048e8375cdb99dd81e58f47
0xMillionz <madokakaname99@protonmail.com>
2022-02-09 13:16:35 -0500

    working theme header lol

--- apps/Dapp/pages/_app.js
+++ apps/Dapp/pages/_app.js
@@ -1,11 +1,18 @@
 import 'styles/globals.css';
 import { useState } from 'react';
 import { WalletKitProvider } from '@gokiprotocol/walletkit';
+
+// Contexts
 import { ThemeProvider } from "next-themes";
 import LayoutContext from '@context/layoutContext';
+import ProtocolContext from '@context/protocolDataContext';
+
 
 function MyApp({ Component, pageProps }) {
   const [ layout, setLayout ] = useState("dashboard");
+  const [ data, setData ] = useState({
+    hlxPrice: 0,
+  }); 
 
   return(
     <WalletKitProvider
@@ -14,11 +21,13 @@ function MyApp({ Component, pageProps }) {
         name: 'Helix'
       }}
     >
-      <ThemeProvider attribute='class'>
-        <LayoutContext.Provider value={{ layout, setLayout }}>
-          <Component {...pageProps} />
-        </LayoutContext.Provider>
-      </ThemeProvider>
+      <ProtocolContext.Provider value={{ data, setData }}>
+        <ThemeProvider attribute='class'>
+          <LayoutContext.Provider value={{ layout, setLayout }}>
+            <Component {...pageProps} />
+          </LayoutContext.Provider>
+        </ThemeProvider>
+      </ProtocolContext.Provider>
     </WalletKitProvider>
   );
 }
