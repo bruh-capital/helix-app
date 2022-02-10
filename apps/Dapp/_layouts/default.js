163aa184f425dab71bbb7d59b6936762035cd6c1
0xMillionz <madokakaname99@protonmail.com>
2022-02-09 23:53:50 -0500

    responsive BG images

--- apps/Dapp/_layouts/default.js
+++ apps/Dapp/_layouts/default.js
@@ -1,11 +1,20 @@
 import MetaTagComponent from "@includes/metatags";
 import Header from "@includes/header";
+import { useTheme } from 'next-themes';
 
 export default function DappLayout(props) {
+	const { theme, setTheme } = useTheme();
+
 	return(
 		<main>
 			<MetaTagComponent />
-			<body className="bg-darkMobileBg bg-cover w-full">
+			<body 
+				className={
+					theme === "light" ? 
+						"bg-lightMobileBg sm:bg-lightDesktopBg" :
+						"bg-darkMobileBg sm:bg-darkDesktopBg"
+				}
+			>
 				<Header />
 			</body>
 		</main>
