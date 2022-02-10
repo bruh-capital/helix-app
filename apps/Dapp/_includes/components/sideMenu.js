b1c074cfaa9306d7d0301d7e4544ac95e89d33e8
0xMillionz <madokakaname99@protonmail.com>
2022-02-09 23:33:21 -0500

    side menu page btns

--- apps/Dapp/_includes/components/sideMenu.js
+++ apps/Dapp/_includes/components/sideMenu.js
@@ -1,8 +1,15 @@
-import { Fragment, useState } from 'react'
+import { Fragment, useContext, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { XIcon } from '@heroicons/react/outline'
+import Image from 'next/image';
 
-export default function SideMenu(props) {
+import { useTheme } from 'next-themes';
+import LayoutContext from '@context/layoutContext';
+
+export default function DappSideMenu(props) {
+	const { theme, setTheme } = useTheme();
+	const { layout, setLayout } = useContext(LayoutContext);
+
 	return (
 		<Transition.Root show={props.open} as={Fragment}>
 			<Dialog as="div" className="fixed inset-0 overflow-hidden" onClose={props.setOpen}>
@@ -29,7 +36,7 @@ export default function SideMenu(props) {
 							leaveTo="translate-x-full"
 						>
 							<div className="relative w-screen max-w-md">
-								<div className="h-full flex flex-col pb-6 bg-white shadow-xl overflow-y-scroll">
+								<div className="h-full flex flex-col pb-6 bg-[#E1E1E1] dark:bg-[#191B1F] shadow-xl overflow-y-scroll">
 									<div className="flex items-center justify-start px-4 pt-4 pb-0">
 										<button
 											type="button"
@@ -39,12 +46,55 @@ export default function SideMenu(props) {
 											<XIcon className="h-6 w-6" aria-hidden="true" />
 										</button>
 									</div>
+									<div className="flex items-center justify-center">
+										<Image 
+											src={ 
+												"/3d/" + 
+												(theme == "light"? 
+												"4K_3D_black.png":
+												"4K_3D_white.png")
+											}
+											height={120}
+											width={120}
+											layout="fixed"
+										/>
+										<div className="hidden sm:flex text-5xl mt-10 my-auto -ml-5 text-[#3E3E3E] dark:text-white font-[Junegull] align-center text-center">HELIX</div>
+									</div>
 									<div className="mt-6 relative flex-1 px-4 sm:px-6">
-										{/* Replace with your content */}
-										<div className="absolute inset-0 px-4 sm:px-6">
-											<div className="h-full border-2 border-dashed border-gray-200" aria-hidden="true" />
+										<div className="absolute content-center items-center inset-0 px-4 sm:px-6">
+											<div className="flex flex-col px-4 py-3 space-y-2 items-center">
+												<button
+													className={
+														"rounded-md text-md px-4 font-semibold " +
+														(layout == "dashboard"?
+															"bg-[#C8C7CA] text-black dark:bg-[#3A3D45] dark:text-white":
+															"bg-transparent text-[#949494]"
+														) 
+													}
+													onClick={() => {layout !== "dashboard" && setLayout("dashboard")}}
+												>Dashboard</button>
+												<button
+													className={
+														"rounded-md text-md px-4 font-semibold " +
+														(layout == "stake"?
+															"bg-[#C8C7CA] text-black dark:bg-[#3A3D45] dark:text-white":
+															"bg-transparent text-[#949494]"
+														) 
+													}
+													onClick={() => {layout !== "stake" && setLayout("stake")}}
+												>Stake</button>
+												<button
+													className={
+														"rounded-md text-md px-4 font-semibold " +
+														(layout == "bond"?
+															"bg-[#C8C7CA] text-black dark:bg-[#3A3D45] dark:text-white":
+															"bg-transparent text-[#949494]"
+														) 
+													}
+													onClick={() => {layout !== "bond" && setLayout("bond")}}
+												>Bond</button>
+											</div>
 										</div>
-										{/* /End replace */}
 									</div>
 								</div>
 							</div>
