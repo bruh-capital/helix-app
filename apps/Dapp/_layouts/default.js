d815110c038dcb8d97789d1a07fb4389468c7a60
0xMillionz <madokakaname99@protonmail.com>
2022-02-10 21:19:23 -0500

    fixed layout

--- apps/Dapp/_layouts/default.js
+++ apps/Dapp/_layouts/default.js
@@ -1,20 +1,23 @@
 import MetaTagComponent from "@includes/metatags";
 import Header from "@includes/header";
 import { useTheme } from 'next-themes';
+import { Html } from "next/document"
 
 export default function DappLayout(props) {
 	const { theme, setTheme } = useTheme();
 
 	return(
-		<main
-			className={
-				theme === "light" ? 
-					"bg-lightMobileBg sm:bg-lightDesktopBg bg-cover" :
-					"bg-darkMobileBg sm:bg-darkDesktopBg bg-cover"
-			}
-		>
+		<>
 			<MetaTagComponent />
-			<Header />
-		</main>
+			<body 
+				className={
+					theme === "light" ? 
+						"bg-lightMobileBg sm:bg-lightDesktopBg bg-cover" :
+						"bg-darkMobileBg sm:bg-darkDesktopBg bg-cover"
+				}
+			>
+				<Header />
+			</body>
+		</>
 	)
 }
