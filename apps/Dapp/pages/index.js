48abf39b36b778c9901b25b604edb3e8d971784d
0xMillionz <madokakaname99@protonmail.com>
2022-02-11 12:19:16 -0500

    updated layouts

--- apps/Dapp/pages/index.js
+++ apps/Dapp/pages/index.js
@@ -1,4 +1,10 @@
 import dynamic from "next/dynamic";
+import { useContext } from "react";
+
+import LayoutContext from "@context/layoutContext";
+import Dash from "@includes/dash";
+import Bond from "@includes/bond";
+import Stake from "@includes/stake";
 
 const PWAprompt = dynamic(
 	() => {
@@ -10,7 +16,14 @@ const PWAprompt = dynamic(
 import DappLayout from "@layouts/default";
 
 export default function AppPage() {
+	const { layout, setLayout } = useContext(LayoutContext);
+
+
 	return(
-		<DappLayout/>
+		<DappLayout>
+			{layout === "dashboard" && <Dash/>}
+			{layout === "stake" && <Stake/>}
+			{layout === "bond" && <Bond/>}
+		</DappLayout>
 	);
 }
