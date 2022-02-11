9903e7beaa558ff5000a5d0c6002140083f1d378
0xMillionz <madokakaname99@protonmail.com>
2022-02-11 13:27:13 -0500

    text leading

--- apps/Dapp/_includes/dash.js
+++ apps/Dapp/_includes/dash.js
@@ -6,14 +6,14 @@ export default function Dash(props) {
 	const { theme, setTheme } = useTheme();
 	return(
 		<div className="h-screen -mt-24 content-center items-center py-36" >
-			<div className="grid relative grid-cols-2 grid-rows-6 lg:grid-rows-3 w-full px-8 md:px-24 h-full space-y-8">
+			<div className="grid relative grid-cols-2 grid-rows-6 lg:grid-rows-3 w-full px-8 md:px-24 xl:px-32 2xl:px-48 h-full space-y-8 justify-around">
 				<div className="flex col-span-2 row-span-1 rounded-xl bg-mesh-1-bg bg-cover">
 					<div className="flex flex-row px-4 xl:px-14 py-auto my-auto md:my-4 lg:my-auto justify-around align-center w-full">
 						<div className="flex flex-col basis-3/4 h-full items-start align-middle">
-							<span className="basis-full md:basis-3/4 text-xl md:text-3xl lg:text-6xl xl:text-7xl max-w-3xl md:my-auto py-2 font-bold text-white">
+							<span className="basis-full leading-none md:basis-3/4 text-xl md:text-3xl lg:text-6xl xl:text-7xl max-w-3xl md:my-auto py-2 font-bold text-white">
 								Helix is exclusively available on Solana.
 							</span>
-							<span className="hidden lg:flex md:hidden sm:flex font-bold basis-1/4 text-xs md:text-xl xl:font-3xl text-white">
+							<span className="flex font-bold basis-1/4 text-xs -mt-3 md:-mt-0 md:text-xl xl:font-3xl text-white">
 								Discover physical and digital marketplaces and more!
 							</span>	
 						</div>
