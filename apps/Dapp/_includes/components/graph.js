6963e2138e03b99aebccd7a1155946a21d810ee3
0xMillionz <madokakaname99@protonmail.com>
2022-02-11 19:41:22 -0500

    dash skeleton

---
+++ apps/Dapp/_includes/components/graph.js
@@ -0,0 +1,44 @@
+import { ArrowSmUpIcon, ArrowSmDownIcon } from "@heroicons/react/outline"
+
+/**
+ * @param {string} props.graphName
+ * @param {string} props.currentGraphValue
+ * @param {} props.
+ * @param {} props.graphData
+ */
+export default function Graph(props) {
+	return(
+		<div className="flex flex-col justify-start my-auto mx-16">
+			<h2 className="text-[#8C8C8C] dark:text-[#9E9E9E] mb-2 gap-y-2">{props.graphName}</h2>
+			<div className="flex flex-row items-center justify-between">
+				<span className="text-[#474747] dark:text-white font-bold text-xl md:text-3xl">
+					{props.graphCurrentValue || "N/A"}	
+				</span>
+				<div>
+					{
+						(props?.graphChange && props.graphChange >= 0)  ? (
+							<div className="flex flex-row">
+								<span className="text-green-800">{(props?.graphChange || "N/A") + "%"}</span>
+								<ArrowSmUpIcon className="h-4 w-4 text-green-800"/>
+							</div>
+						) : (
+							<div className="flex flex-row">
+								<span className="text-red-800">{(props?.graphChange || "N/A") + "%"}</span>
+								<ArrowSmDownIcon className="h-4 w-4 text-red-800"/>
+							</div>
+						)
+					}
+				</div>
+			</div>
+			<div className="flex-grow">
+				{
+					props.graphData ? (
+						<div className="flex flex-grow bg-[#C0C0C0] animate-pulse px-24"/>
+					) : (
+						<div className="rounded-md bg-[#C0C0C0] animate-pulse px-24 py-48 m-auto"></div>
+					)
+				}
+			</div>
+		</div>
+	);
+}
--- apps/Dapp/_includes/dash.js
+++ apps/Dapp/_includes/dash.js
@@ -1,20 +1,26 @@
 import Image from "next/image";
+import { useContext } from "react";
+import Stat from "@includes/components/stat";
+import Graph from "@includes/components/graph";
 
 import { useTheme } from "next-themes";
+import ProtocolContext from "@context/protocolDataContext";
+
 
 export default function Dash(props) {
 	const { theme, setTheme } = useTheme();
+	const { data, setData } = useContext(ProtocolContext);
 	return(
-		<div className="h-screen -mt-24 content-center items-center py-36" >
-			<div className="grid relative grid-cols-2 grid-rows-6 lg:grid-rows-3 w-full px-8 md:px-24 xl:px-32 2xl:px-48 h-full space-y-8 justify-around">
-				<div className="flex col-span-2 row-span-1 rounded-xl bg-mesh-1-bg bg-cover">
+		<div className="h-screen -mt-24 content-center items-center pt-32 md:pt-36 pb-24" >
+			<div className="grid gap-x-24 grid-cols-2 grid-rows-6 lg:grid-rows-4 w-full px-8 md:px-24 xl:px-32 2xl:px-52 h-full space-y-8 justify-around">
+				<div className="flex col-span-2 row-span-1 rounded-xl bg-mesh-1-bg bg-cover overflow-hidden">
 					<div className="flex flex-row px-4 xl:px-14 py-auto my-auto md:my-4 lg:my-auto justify-around align-center w-full">
-						<div className="flex flex-col basis-3/4 h-full items-start align-middle">
-							<span className="basis-full leading-none md:basis-3/4 text-xl md:text-3xl lg:text-6xl xl:text-7xl max-w-3xl md:my-auto py-2 font-bold text-white">
+						<div className="flex flex-col basis-3/4 h-full items-start align-middle my-auto">
+							<span className="basis-full leading-none md:basis-3/4 text-xl md:text-3xl lg:text-6xl xl:text-6xl max-w-3xl md:my-auto py-2 font-bold text-white">
 								Helix is exclusively available on Solana.
 							</span>
-							<span className="flex font-bold basis-1/4 text-xs -mt-1 md:-mt-0 md:text-xl xl:font-3xl text-white leading-none">
-								Discover physical and digital marketplaces and more!
+							<span className="flex font-bold basis-1/4 text-xs -mt-1 md:text-xl xl:font-3xl text-white leading-none">
+								Discover privacy marketplaces and more!
 							</span>	
 						</div>
 						<div className="justify-center basis-1/4 align-middle my-auto">
@@ -22,8 +28,8 @@ export default function Dash(props) {
 							<div className="hidden lg:flex justify-center place-content-center">
 								<Image 
 									src="/dapp-assets/Card/1/stats.png"
-									height={175}
-									width={181}
+									height={150}
+									width={155}
 									layout="fixed"
 								/>
 							</div>
@@ -38,11 +44,31 @@ export default function Dash(props) {
 						</div>
 					</div>
 				</div>
-				<div className="col-span-1 row-span-3 flex flex-col">
-					<div className=" outline-1 outline-[#A5A5A5] dark:outline-[#383838] bg-[#D9D8E2] dark:bg-[#191B1F]"></div>
+				<div className="flex flex-col col-span-2 md:col-span-1 row-span-1 rounded-xl border-2 border-[#A5A5A5] dark:border-[#383838] bg-[#D9D8E2] dark:bg-[#191B1F]">
+					<Stat
+						statName="Revenue (24HR)"
+						statValue={data?.revenue}
+						statChange={data?.revenueChange}
+					/>
 				</div>
-				<div className="col-span-1 row-span-1 rounded-md">
-
+				<div className="flex flex-col col-span-2 md:col-span-1 row-span-1 rounded-xl border-2 border-[#A5A5A5] dark:border-[#383838] bg-[#D9D8E2] dark:bg-[#191B1F]">
+					<Stat 
+						statName="HLX Staked (USDC)"
+						statValue={data?.stakedHlx}
+						statChange={false}
+					/>
+				</div>
+				<div className="flex flex-col col-span-2 md:col-span-1 row-span-1 rounded-xl border-2 border-[#A5A5A5] dark:border-[#383838] bg-[#D9D8E2] dark:bg-[#191B1F]">
+					<Stat 
+						statName="Staking APY (%)"
+						statValue={data?.stakedHlx}
+						statChange={false}
+					/>
+				</div>
+				<div className="flex col-start-2 row-start-2 flex-col col-span-1 row-span-3 rounded-xl border-2 border-[#A5A5A5] dark:border-[#383838] bg-[#D9D8E2] dark:bg-[#191B1F]">
+					<Graph
+						graphName="TVL"
+					/>
 				</div>
 			</div>
 		</div>
