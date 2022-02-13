779d4ce3a35975ba91680aff641bc5175179ad0c
0xMillionz <madokakaname99@protonmail.com>
2022-02-13 16:53:49 -0500

    scuffed bonding stats

--- apps/Dapp/_includes/bond.js
+++ apps/Dapp/_includes/bond.js
@@ -1,6 +1,20 @@
+import Graph from "@includes/components/graph";
+
 export default function Bond(props) {
 	return(
-		<div className="h-screen -mt-24 content-center items-center pt-32 md:pt-36 pb-24">
+		<div className="-mt-24 content-center items-center pt-32 md:pt-36 pb-24">
+			<div className="grid gap-x-24 grid-cols-2 grid-rows-6 lg:grid-rows-4 w-full px-8 md:px-24 xl:px-32 2xl:px-64 h-full justify-around">
+				<div className="flex col-start-1 py-6 row-start-1 flex-col col-span-1 lg:col-span-1 row-span-3 rounded-xl border-2 border-[#A5A5A5] dark:border-[#383838] bg-[#D9D8E2] dark:bg-[#191B1F]">
+					<Graph 
+						graphName="Total Value Deposited"
+					/>
+				</div>
+				<div className="flex col-start-2 py-6 row-start-1 flex-col col-span-1 lg:col-span-1 row-span-3 rounded-xl border-2 border-[#A5A5A5] dark:border-[#383838] bg-[#D9D8E2] dark:bg-[#191B1F]">
+					<Graph 
+						graphName="HLX Price"
+					/>
+				</div>
+			</div>
 		</div>
 	);
 }
