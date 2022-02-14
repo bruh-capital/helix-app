282c430af2f08cf1d6aca51d2fc9fdbaa7ba79a1
0xMillionz <madokakaname99@protonmail.com>
2022-02-14 12:44:14 -0500

    spacing fixes, modals

--- apps/Dapp/_includes/dash.js
+++ apps/Dapp/_includes/dash.js
@@ -11,8 +11,8 @@ export default function Dash(props) {
 	const { theme, setTheme } = useTheme();
 	const { data, setData } = useContext(ProtocolContext);
 	return(
-		<div className="h-screen -mt-24 content-center items-center pt-32 md:pt-36 pb-24" >
-			<div className="grid gap-x-24 grid-cols-2 grid-rows-6 lg:grid-rows-4 w-full px-8 md:px-24 xl:px-32 2xl:px-64 h-full space-y-8 justify-around">
+		<div className="h-screen -mt-24 content-center items-center pt-28 md:pt-36 pb-0 md:pb-24" >
+			<div className="grid gap-x-24 grid-cols-2 grid-rows-6 lg:grid-rows-4 w-full px-8 md:px-24 xl:px-32 2xl:px-64 h-full space-y-2 md:space-y-8 justify-around">
 				<div className="flex col-span-2 row-span-1 rounded-xl bg-mesh-1-bg bg-cover overflow-hidden">
 					<div className="flex flex-row px-4 xl:px-14 py-auto my-auto md:my-4 lg:my-auto justify-around align-center w-full">
 						<div className="flex flex-col basis-3/4 h-full items-start align-middle my-auto">
@@ -55,14 +55,14 @@ export default function Dash(props) {
 					<Stat 
 						statName="HLX Staked (USDC)"
 						statValue={data?.stakedHlx}
-						statChange={false}
+						statChange={data?.stakedHlxChange}
 					/>
 				</div>
 				<div className="flex flex-col col-span-2 lg:col-span-1 row-span-1 rounded-xl border-2 border-[#A5A5A5] dark:border-[#383838] bg-[#D9D8E2] dark:bg-[#191B1F]">
 					<Stat 
 						statName="Staking APY (%)"
-						statValue={data?.stakedHlx}
-						statChange={false}
+						statValue={data?.stakingApy}
+						statChange={data?.stakingApyChange}
 					/>
 				</div>
 				<div className="flex col-start-1 lg:md:col-start-2 row-start-2 flex-col col-span-2 lg:col-span-1 row-span-3 rounded-xl border-2 border-[#A5A5A5] dark:border-[#383838] bg-[#D9D8E2] dark:bg-[#191B1F]">
