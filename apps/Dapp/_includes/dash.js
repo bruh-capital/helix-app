6145675d9a28238b1c6515ad99cdc157af184829
0xMillionz <madokakaname99@protonmail.com>
2022-02-14 23:06:21 -0500

    grid spacing on mobile fixed

--- apps/Dapp/_includes/dash.js
+++ apps/Dapp/_includes/dash.js
@@ -11,9 +11,9 @@ export default function Dash(props) {
 	const { theme, setTheme } = useTheme();
 	const { data, setData } = useContext(ProtocolContext);
 	return(
-		<div className="h-screen -mt-24 content-center items-center pt-28 md:pt-36 pb-0 md:pb-24" >
-			<div className="grid gap-x-24 grid-cols-2 grid-rows-6 lg:grid-rows-4 w-full px-8 md:px-24 xl:px-32 2xl:px-64 h-full space-y-2 md:space-y-8 justify-around">
-				<div className="flex col-span-2 row-span-1 rounded-xl bg-mesh-1-bg bg-cover overflow-hidden">
+		<div className="h-screen -mt-24 content-center items-center pt-28 md:pt-36 pb-8 md:pb-24" >
+			<div className="grid gap-x-0 md:gap-x-24 grid-cols-2 grid-rows-7 lg:grid-rows-4 w-full px-8 md:px-24 xl:px-32 2xl:px-64 h-full space-y-2 md:space-y-8 justify-around">
+				<div className="flex col-span-2 row-span-1 rounded-xl bg-mesh-1-bg bg-cover">
 					<div className="flex flex-row px-4 xl:px-14 py-auto my-auto md:my-4 lg:my-auto justify-around align-center w-full">
 						<div className="flex flex-col basis-3/4 h-full items-start align-middle my-auto">
 							<span className="basis-full leading-none md:basis-3/4 text-xl md:text-3xl lg:text-6xl xl:text-6xl max-w-3xl md:my-auto py-2 font-bold text-white">
