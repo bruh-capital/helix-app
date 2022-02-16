955d74ab92147351cf8e9095d88ec69ed549891f
0xMillionz <madokakaname99@protonmail.com>
2022-02-16 13:35:43 -0500

    fixed carousel display

--- apps/Dapp/_includes/dash.js
+++ apps/Dapp/_includes/dash.js
@@ -13,7 +13,7 @@ export default function Dash(props) {
 	return(
 		<div className="h-screen -mt-24 content-center items-center pt-28 md:pt-36 pb-8 md:pb-24" >
 			<div className="grid gap-x-0 md:gap-x-24 grid-cols-2 grid-rows-7 lg:grid-rows-4 w-full px-8 md:px-24 xl:px-32 2xl:px-64 h-full space-y-2 md:space-y-8 justify-around">
-			<CardCarousel className="w-screen"/>
+				<CardCarousel className="w-screen"/>
 				<div className="flex flex-col col-span-2 lg:col-span-1 row-span-1 rounded-xl border-2 border-[#A5A5A5] dark:border-[#383838] bg-[#D9D8E2] dark:bg-[#191B1F]">
 					<Stat
 						statName="Revenue (24HR)"
