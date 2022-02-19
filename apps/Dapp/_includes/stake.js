cb48adbe727489cb2eabecf9cab02d87cf6675c5
0xMillionz <madokakaname99@protonmail.com>
2022-02-19 01:08:05 -0500

    stake changes for spacing on mobile

--- apps/Dapp/_includes/stake.js
+++ apps/Dapp/_includes/stake.js
@@ -35,12 +35,12 @@ export default function Stake(props) {
 
 	return(
 		<div className="-mt-24 content-center items-center pt-32 md:pt-36 pb-24">
-			<div className="flex flex-col sm:px-0 md:px-28 lg:px-50 xl:px-70 2xl:px-96 sm:mx-20 gap-y-8">
+			<div className="flex flex-col mx-8 gap-y-8">
 				<div className="flex flex-row justify-items-start rounded-xl bg-[#D9D8E2] dark:bg-[#191B1F] border-2 border-[#BABABA] dark:border-[#383838] w-full">
-					<div className="rounded-xl p-2 m-2 bg-[#EEEEEE] bg-opacity-60 dark:bg-[#383838]">
+					<div className="rounded-xl p-2 m-2 bg-[#EEEEEE] bg-opacity-60 dark:bg-[#343A45]">
 						<InformationCircleIcon className="h-5 w-5"/>
 					</div>
-					<div className="text-sx md:text-md font-medium text-[#606060] dark:text-[#7C849A] my-auto whitespace-nowrap truncate text-sm">
+					<div className="text-sx md:text-medium font-medium text-[#606060] dark:text-[#7C849A] my-auto whitespace-nowrap truncate text-sm">
 						Note: The "Configure" transaction is only needed when interacting for the first time.
 					</div>
 				</div>
@@ -53,11 +53,11 @@ export default function Stake(props) {
 					<div className="flex font-bold text-[#272629] dark:text-white px-10 md:px-16 text-3xl md:text-6xl">
 						{typeof stakedBalance == "number" ? stakedBalance : "N/A"}
 					</div>
-					<div className="flex flex-row self-center justify-around font-bold text-xl my-8 gap-x-8 md:gap-x-16">
+					<div className="flex flex-row self-center justify-around font-bold text-lg md:text-xl my-4 gap-x-8 md:gap-x-16">
 						<button
 							className={
 								"rounded-lg px-4 py-2 " + (uiFunction === "stake" ?
-									"bg-[#343A45] text-white" : "bg-transparent text-[#696B70] hover:bg-indigo-900 hover:text-gray-300")
+									"bg-[#343A45] text-white" : "bg-transparent text-[#696B70] hover:text-gray-300")
 							}
 							onClick={() => setUiFunction("stake")}
 						>
@@ -66,12 +66,12 @@ export default function Stake(props) {
 						<button
 							className={
 								"rounded-lg px-4 py-2 " + (uiFunction === "unstake" ?
-									"bg-[#343A45] text-white" : "bg-transparent text-[#696B70] hover:bg-indigo-900 hover:text-gray-300")
+									"bg-[#343A45] text-white" : "bg-transparent text-[#696B70] dark:hover: hover:text-gray-300")
 							}
 							onClick={() => setUiFunction("unstake")}
 						>Unstake</button>
 					</div>
-					<div className="flex flex-col rounded-lg mx-10 md:mx-16 p-4 mb-4 bg-[#C0C0C0] dark:bg-[#212429]">
+					<div className="flex flex-col rounded-lg mx-6 md:mx-16 p-3 mb-4 bg-[#C0C0C0] dark:bg-[#212429]">
 						<div className="grid grid-cols-2 mb-2">
 							<Image
 								src = {"/2d/" + (theme == "light"? "2d_logo3.png" : "2d_logo4.png")}
@@ -89,7 +89,7 @@ export default function Stake(props) {
 							/>
 						</div>
 						<div className="text-xs text-slate-500">
-							Total sHLX Received: {protocolData ? amount/(protocolData.shareRatio.toNumber()/1000) : null}
+							Total: {protocolData ? amount/(protocolData.shareRatio.toNumber()/1000) : null}
 						</div>
 
 					</div>
