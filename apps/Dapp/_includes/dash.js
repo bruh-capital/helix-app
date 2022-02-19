c36902d36f87917685c094be0b7a187b81263c0d
0xMillionz <madokakaname99@protonmail.com>
2022-02-19 17:20:46 -0500

    mobile fix dash, desktop fix stake

--- apps/Dapp/_includes/dash.js
+++ apps/Dapp/_includes/dash.js
@@ -8,12 +8,11 @@ import ProtocolContext from "@context/protocolDataContext";
 import CardCarousel from "@includes/components/cardCarousel";
 
 export default function Dash(props) {
-	const { theme, setTheme } = useTheme();
 	const { data, setData } = useContext(ProtocolContext);
 
 	return(
 		<div className="h-screen items-center mt-4 lg:mt-10 lg:pb-36" >
-			<div className="grid grid-cols-1 lg:grid-cols-2 grid-rows-8 lg:grid-rows-4 w-full px-8 md:px-24 xl:px-32 2xl:px-64 h-full gap-4 lg:gap-8">
+			<div className="grid grid-cols-1 lg:grid-cols-2 grid-rows-8 lg:grid-rows-4 w-full px-8 md:px-24 xl:px-32 2xl:px-64 h-full gap-2 sm:gap-4 lg:gap-8">
 				<div className="h-full w-full row-start-1 row-span-1 col-span-1 lg:col-span-2">
 					<CardCarousel />
 				</div>
