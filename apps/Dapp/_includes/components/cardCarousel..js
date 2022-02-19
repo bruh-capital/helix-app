4bf7d130ca86e3b4813c2dfc4bd02eac09335afd
0xMillionz <madokakaname99@protonmail.com>
2022-02-19 00:17:52 -0500

    finally someone made a nice carousel component...

--- apps/Dapp/_includes/components/cardCarousel..js
+++ apps/Dapp/_includes/components/cardCarousel..js
@@ -1,22 +1,10 @@
 import { AvailableCard } from '@includes/components/cards/cards';
-import { Carousel } from 'react-responsive-carousel';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Rerousel } from 'rerousel';
 
-export default function CardCarousel() {
+export default function CardCarousel(props) {
 	return(
-		<Carousel 
-			autoPlay
-			infiniteLoop
-			stopOnHover
-			showStatus={false}
-			showArrows={false}
-			showThumbs={false}
-			interval={7000}
-			className="-mb-12"
-		>
+		<Rerousel>
 			<AvailableCard />
-			<AvailableCard />
-			<AvailableCard />
-		</Carousel>
+		</Rerousel>
 	);
 }
