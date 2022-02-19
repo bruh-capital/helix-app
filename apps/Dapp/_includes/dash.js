54b3c792ae5617708aae03441a428e6fc3c90c62
0xMillionz <madokakaname99@protonmail.com>
2022-02-19 00:54:22 -0500

    dash spacing fixed

--- apps/Dapp/_includes/dash.js
+++ apps/Dapp/_includes/dash.js
@@ -10,32 +10,35 @@ import CardCarousel from "@includes/components/cardCarousel.";
 export default function Dash(props) {
 	const { theme, setTheme } = useTheme();
 	const { data, setData } = useContext(ProtocolContext);
+
 	return(
-		<div className="h-screen items-center mt-4 md:mt-10" >
-			<div className="grid grid-cols-1 grid-rows-8 w-full px-8 md:px-24 xl:px-32 2xl:px-64 h-full space-y-4 md:space-y-8">
-				<CardCarousel />
-				<div className="flex flex-col rounded-xl border-2 border-[#A5A5A5] dark:border-[#383838] bg-[#D9D8E2] dark:bg-[#191B1F]">
+		<div className="h-screen items-center mt-4 lg:mt-10 lg:pb-36" >
+			<div className="grid grid-cols-1 lg:grid-cols-2 grid-rows-8 lg:grid-rows-4 w-full px-8 md:px-24 xl:px-32 2xl:px-64 h-full gap-4 lg:gap-8">
+				<div className="h-full w-full row-start-1 row-span-1 col-span-1 lg:col-span-2">
+					<CardCarousel />
+				</div>
+				<div className="flex flex-col row-start-2 col-start-1 col-span-1 rounded-xl border-2 border-[#A5A5A5] dark:border-[#383838] bg-[#D9D8E2] dark:bg-[#191B1F]">
 					<Stat
 						statName="Revenue (24HR)"
 						statValue={data?.revenue}
 						statChange={data?.revenueChange}
 					/>
 				</div>
-				<div className="flex flex-col rounded-xl border-2 border-[#A5A5A5] dark:border-[#383838] bg-[#D9D8E2] dark:bg-[#191B1F]">
+				<div className="flex flex-col row-start-3 col-start-1 col-span-1 rounded-xl border-2 border-[#A5A5A5] dark:border-[#383838] bg-[#D9D8E2] dark:bg-[#191B1F]">
 					<Stat 
 						statName="HLX Staked (USDC)"
 						statValue={data?.stakedHlx}
 						statChange={data?.stakedHlxChange}
 					/>
 				</div>
-				<div className="flex flex-col rounded-xl border-2 border-[#A5A5A5] dark:border-[#383838] bg-[#D9D8E2] dark:bg-[#191B1F]">
+				<div className="flex flex-col row-start-4 col-start-1 col-span-1 rounded-xl border-2 border-[#A5A5A5] dark:border-[#383838] bg-[#D9D8E2] dark:bg-[#191B1F]">
 					<Stat 
 						statName="Staking APY (%)"
 						statValue={data?.stakingApy}
 						statChange={data?.stakingApyChange}
 					/>
 				</div>
-				<div className="flex flex-col rounded-xl border-2 border-[#A5A5A5] dark:border-[#383838] bg-[#D9D8E2] dark:bg-[#191B1F]">
+				<div className="flex flex-col row-start-5 lg:row-start-2 col-span-1 row-span-3 rounded-xl border-2 border-[#A5A5A5] dark:border-[#383838] bg-[#D9D8E2] dark:bg-[#191B1F]">
 					<Graph
 						graphName="TVL"
 					/>
