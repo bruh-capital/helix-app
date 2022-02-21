3d35f0d8a926eded348781013629ccc2613cd728
0xMillionz <madokakaname99@protonmail.com>
2022-02-21 15:57:36 -0500

    hero button pink fixed

--- apps/Landing/_includes/hero.js
+++ apps/Landing/_includes/hero.js
@@ -20,9 +20,9 @@ export default function Hero(props) {
 						<button className="bg-black bg-opacity-70 text-white text-sm md:text-xl px-8 py-4 font-normal border-solid border-4 border-[#5E5FFF] rounded-2xl shadow-blue-glow-md ease-in-out duration-150 hover:shadow-blue-glow-lg hover:scale-105">
 							Enter App
 						</button>
-						<button className="bg-black bg-opacity-70 text-white text-sm md:text-xl px-8 py-4 font-normal border-solid border-4 border-[#FB60FF] rounded-2xl shadow-pink-glow-md ease-in-out duration-150 hover:shadow-pink-glow-lg hover:scale-105">
+						<button className="bg-black bg-opacity-70 text-white text-sm md:text-xl px-8 py-4 font-normal border-solid border-4 border-[#ca1aff] rounded-2xl shadow-pink-glow-md ease-in-out duration-150 hover:shadow-pink-glow-lg hover:scale-105">
 							Marketplace
-							<span className="absolute inline-flex items-center justify-center px-3 py-2 text-sm font-normal leading-none text-white bg-[#FB60FF] rounded-full -translate-y-7 translate-x-1">WIP</span>	
+							<span className="absolute inline-flex items-center justify-center px-3 py-2 text-sm font-normal leading-none text-white bg-[#CA1aFF] rounded-full -translate-y-7 translate-x-1">WIP</span>	
 						</button>
 					</div>
 				</div>
