ac9e89f3602c08e0710854d26744df4c47d502fe
0xDEADBEAT <93951859+0xDEADBEAT@users.noreply.github.com>
2022-02-21 03:01:13 -0500

    YO

--- apps/Landing/_includes/socials.js
+++ apps/Landing/_includes/socials.js
@@ -4,30 +4,30 @@ export default function Socials(props) {
 	return(
 		<div className="flex flex-row justify-around w-11/12 md:w-3/5 h-screen mx-auto gap-x-8">
 			<div className="flex flex-col justify-center items-center basis-1/2 h-1/12 my-auto">
-				<div className="flex font-bold text-white text-2xl md:text-6xl basis-2/3 text-left">
-					Keep up with HelixDAO!
+				<div className="flex font-bold text-white text-2xl md:text-6xl basis-2/3 text-left font-sans">
+					Stay updated on the project!
 				</div>
 				<div className="flex font-semibold text-xs md:text-xl text-[#7E8083] text-left">Join the community and stay up to date on what's happening in Helix</div>
 			</div>
 			<div className="flex flex-col justify-center items-center basis-1/2 gap-y-3">
 				<div className="flex flex-row px-2 w-full rounded-lg text-white bg-[#101010]">
 					<div className="flex flex-col justify-center items-center">
-						<Image/>
+						<Image src="/landingassets/landingpage/icons/twitter.png" height={29} width={29} />
 					</div>
 				</div>
 				<div className="flex flex-row px-2 w-full rounded-lg text-white bg-[#101010]">
 					<div className="flex flex-col justify-center items-center">
-						<Image/>
+						<Image src="/landingassets/landingpage/icons/Vector.png" height={25} width={25} />
 					</div>
 				</div>
 				<div className="flex flex-row px-2 w-full rounded-lg text-white bg-[#101010]">
 					<div className="flex flex-col justify-center items-center">
-						<Image/>
+						<Image src="/landingassets/landingpage/icons/Frame 1.png" height={32} width={32} />
 					</div>
 				</div>
 				<div className="flex flex-row px-2 w-full rounded-lg text-white bg-[#101010]">
 					<div className="flex flex-col justify-center items-center">
-						<Image/>
+						<Image src="/landingassets/landingpage/icons/image 24.png" height={29} width={29} />
 					</div>
 				</div>
 			</div>
--- apps/Landing/_includes/timeline.js
+++ apps/Landing/_includes/timeline.js
@@ -18,7 +18,7 @@ export default function TimeLine(props) {
 				<div className="flex flex-row w-11/12">
 					<div className="relative basis-1/3 mx-auto text-center">
 							<a
-								href="./LitePaper.pdf"
+								href="./Litepaper.pdf"
 							>
 								<div className="rounded-lg mx-auto w-3/4 lg:w-1/3 -mt-6 flex flex-col items-center justify-center border md:border-2 border-[#C95DF1] bg-black shadow-pink-glow-md hover:shadow-pink-glow-lg hover:ease-in duration-75 text-white text-s font-semibold py-4">
 									
