445bb38e153809e36f02669c7256bfc928484e5d
0xMillionz <madokakaname99@protonmail.com>
2022-02-21 15:55:31 -0500

    NEW IMAGE AYOO

--- apps/Landing/_includes/components/featureCard.js
+++ apps/Landing/_includes/components/featureCard.js
@@ -2,7 +2,7 @@ import { loadGetInitialProps } from "next/dist/shared/lib/utils";
 
 export default function FeatureCard(props) {
 	return(
-		<div className="p-4 basis-1/3 bg-[#101010] rounded-lg">
+		<div className="p-4 basis-1/3 bg-[#101010] rounded-xl">
 			<div className="hidden md:flex items-center justify-center align-middle content-center">
 				{props.image}
 			</div>
