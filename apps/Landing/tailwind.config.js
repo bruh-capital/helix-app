aa39dcdcf1d3c9348c2c5b416a3918e2204e971d
0xMillionz <madokakaname99@protonmail.com>
2022-02-20 19:19:20 -0500

    bruh

--- apps/Landing/tailwind.config.js
+++ apps/Landing/tailwind.config.js
@@ -9,13 +9,14 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        "tokenbg": "url('/landingassets/landingpage/bg/tokens.png')",
+        "tokenfallingpreview": "url('/landingassets/banners/fallingpreview.png')",
+        "tokenfalling": "url('/landingassets/banners/tokenfalling.gif')",
       },
       boxShadow: {
-        'pink-glow-md': '0px 0px 20px #E66EE9',
-        'pink-glow-lg': '0px 0px 40px #E66EE9',
-        'blue-glow-md': '0px 0px 20px #59A6F4',
-        'blue-glow-lg': '0px 0px 40px #59A6F4',
+        'pink-glow-md': '0px 0px 30px #E66EE9',
+        'pink-glow-lg': '0px 0px 60px #E66EE9',
+        'blue-glow-md': '0px 0px 30px #59A6F4',
+        'blue-glow-lg': '0px 0px 60px #59A6F4',
       },
     },
   },
