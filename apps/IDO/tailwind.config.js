55cc275239da107eb9fef6378f04fd2fbbbf8ae5
0xDEADBEAT <93951859+0xDEADBEAT@users.noreply.github.com>
2022-02-22 00:13:36 -0500

    :)

--- apps/IDO/tailwind.config.js
+++ apps/IDO/tailwind.config.js
@@ -19,6 +19,15 @@ module.exports = {
       "mesh-2-bg": "url('/dapp-assets/Card/2/mesh.png')",
       "mesh-3-bg": "url('/dapp-assets/Card/3/mesh.png')",
     },
+    boxShadow: {
+      'pink-glow-md': '0px 0px 30px #E66EE9',
+      'pink-glow-lg': '0px 0px 60px #E66EE9',
+      'blue-glow-md': '0px 0px 30px #59A6F4',
+      'blue-glow-lg': '0px 0px 60px #59A6F4',
+      'gray-glow-md': '0px 0px 30px #6c7278',
+      'gray-glow-lg': '0px 0px 60px #6c7278',
+    },
   },
-  variants: {}
+  variants: {},
+  
 }
