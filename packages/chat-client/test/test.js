7dd04a059a30491288d048b70d4888aa7bb89cb7
0xMillionz <madokakaname99@protonmail.com>
2022-04-15 13:40:04 -0400

    working chat tests

--- packages/chat-client/test/test.js
+++ packages/chat-client/test/test.js
@@ -8,8 +8,11 @@ it('sender', async () => {
 	const testClient = new ChatClient(wallet);
 
 	try {
-		await testClient.sendMessage("hello world" ,"fakepubkey2", true);
 		await testClient.channelSubscribe("fakepubkey2", true);
+		await new Promise(r => setTimeout(r, 2000));
+		await testClient.sendMessage("hello world" ,"fakepubkey2", true);
+		await testClient.sendMessage("hello world2" ,"fakepubkey2", true);
+		await testClient.sendMessage("hello world3" ,"fakepubkey2", true);
 	} finally {
 		await testClient.kafkaProducer.disconnect();
 		await testClient.kafkaConsumer.disconnect();
