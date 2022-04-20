822dc61b82cb1c5fe365795e55fdc6c822d3ca47
0xMillionz <madokakaname99@protonmail.com>
2022-04-19 22:37:59 -0400

    added dummy ssr

--- apps/Market/pages/index.js
+++ apps/Market/pages/index.js
@@ -5,3 +5,26 @@ export default function IndexPage() {
     <HomeLayout />
 	);
 }
+
+export async function getStaticProps() {
+  // FETCHING MOST RECENTLY LISTED ITEMS
+  // do some magic fetching with the solana client later
+  // for now we just create a dummy state...
+  let recentlyListedItems = [
+    {
+      accountAddress: "0x1",
+      title: "Item 1",
+      arweaveImageURl: "https://helixdao.org/landingassets/3dlogos/4K_3D_white.png"
+    },
+    {
+      accountAddress: "0x2",
+      title: "Item 2",
+      arweaveImageURl: "https://helixdao.org/landingassets/3dlogos/4K_3D_white.png"
+    },
+    {
+      accountAddress: "0x3",
+      title: "Item 3",
+      arweaveImageURl: "https://helixdao.org/landingassets/3dlogos/4K_3D_white.png"
+    },
+  ];
+}
