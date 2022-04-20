0b2e2877915009dfada9b2d4a0be93f0beeee4a1
0xMillionz <madokakaname99@protonmail.com>
2022-04-19 20:05:34 -0400

    new market app

---
+++ apps/Market/next.config.js
@@ -0,0 +1,34 @@
+const withTM = require('next-transpile-modules')([
+  '@gokiprotocol/walletkit',
+  '@solana/wallet-adapter-react',
+  '@solana/wallet-adapter-base',
+  'helix-clients',
+]);
+
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
+
+module.exports =  withPWA(
+  withTM({
+    reactStrictMode: true,
+    webpack5: true,
+    webpack: (config) => {
+      config.resolve.fallback = { 
+        fs: false,
+        os: false,
+        path: false,
+        crypto: false,
+      };
+
+      return config;
+    },
+    pwa: {
+      disable: process.env.NODE_ENV === 'development',
+      dest: 'public',
+      runtimeCaching,
+    },
+    images: {
+      domains: ['raw.githubusercontent.com'],
+    }
+  })
+);
