0e03868eb0b1eae42f292dba11f287df8c394c68
0xDEADBEAT <0xDEADBEAT@gmail.com>
2022-04-29 18:01:39 -0400

    transition

--- apps/Market/_includes/components/searchbar.js
+++ apps/Market/_includes/components/searchbar.js
@@ -1,7 +1,6 @@
 import { CheckIcon, SearchIcon } from '@heroicons/react/solid'
-import { Listbox, Popover } from "@headlessui/react";
+import { Listbox } from "@headlessui/react";
 import { useState } from "react";
-import { atom, useRecoilValue } from "recoil";
 
 
 // import {searchProducts}
@@ -24,35 +23,40 @@ export default function Searchbar(props){
     const [categorySelection, setCategorySelection] = useState(categoriesList[0]);
 
     return (
-        <div className='flex flex-row justify-evenly w-1/2 z-10 border-2 m-4 border-gray-200 rounded-xl'>
+        <div className='flex flex-row justify-evenly w-1/2 z-10 border-2 m-4 border-gray-200 rounded-xl min-h-max max-h-10'>
             <input
                 type="text"
                 placeholder="search in marketplace"
-                className='bg-transparent grow m-2'
+                className='bg-transparent focus:outline-none m-2 grow'
                 value = {searchQuery}
                 onChange = {(e)=>{setSearchQuery(e.target.value)}}
             />
             
-            <div className='flex flex-row justify-evenly w-1/3'>
-                <Listbox value={categorySelection} onChange={setCategorySelection}>
-                    <Listbox.Button>
-                        {categorySelection}
-                    </Listbox.Button>
-                    <Listbox.Options>
-                    {categoriesList?.map((cat)=>{
-                        return <Listbox.Option key={cat} value={cat}>
-                            {({ active, selected }) => (
-                                <li className={active ? 'bg-gray-800 text-white' : 'bg-gray-500 text-white'} >
-                                    {selected && <CheckIcon
-                                    width={10}
-                                    height={10} />}
-                                    {cat}
-                                </li>
-                            )}
-                        </Listbox.Option>
-                    })}
-                    </Listbox.Options>
-                </Listbox>
+            <div className='flex flex-row justify-evenly w-2/5'>
+                <div className='max-w-xs w-full h-full flex'>
+                    <Listbox
+                        as="div"
+                        value={categorySelection}
+                        onChange={setCategorySelection}
+                        className="space-y-2 grid grid-cols-1 static w-full h-full justify-center"
+                    >
+                        <Listbox.Button>
+                            {categorySelection}
+                        </Listbox.Button>
+                        <Listbox.Options>
+                        {categoriesList?.map((cat)=>{
+                            return <Listbox.Option key={cat} value={cat}>
+                                {({ active, selected }) => (
+                                    <li className={"flex flex-row justify-center place-items-center gap-x-2 " + (active ? 'bg-gray-800 text-white' : 'bg-gray-500 text-white')} >
+                                        {selected && <CheckIcon width={10} height={10}/>}
+                                        {cat}
+                                    </li>
+                                )}
+                            </Listbox.Option>
+                        })}
+                        </Listbox.Options>
+                    </Listbox>
+                </div>
                 
                 <button
                     onClick = {()=>{searchProducts(searchQuery, categorySelection)}}
