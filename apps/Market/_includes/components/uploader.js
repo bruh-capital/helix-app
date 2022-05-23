c3ffa655690a90a727e25e8ceabf178bd4cd9e77
0xDEADBEAT <0xDEADBEAT@gmail.com>
2022-05-22 22:46:54 -0400

    BARE BONE UPLOADER

--- apps/Market/_includes/components/uploader.js
+++ apps/Market/_includes/components/uploader.js
@@ -1,27 +1,31 @@
+import { useState } from "react";
+import { useContext } from "react";
+
+import BundlrClientCtx from "@contexts/bundlrClientCtx";
+
 export default function Uploader(){
-    const handleFileClick = () => {
-        var fileInputEl = document.createElement("input");
-        fileInputEl.type = "file";
-        fileInputEl.accept = "image/*";
-        fileInputEl.style.display = "none";
-        document.body.appendChild(fileInputEl);
-        fileInputEl.addEventListener("input", function (e) {
-          handleUpload(e);
-          document.body.removeChild(fileInputEl);
-        });
-        fileInputEl.click();
-      };
-    
-      const handleUpload = async (evt) => {
-        let files = evt.target.files;
-        let reader = new FileReader();
-        if (files && files.length > 0) {
-          reader.onload = function () {
-            if (reader.result) {
-              setImg(Buffer.from(reader.result));
-            }
-          };
-          reader.readAsArrayBuffer(files[0]);
-        }
-      };
+  const {bundlrClient, setBundlrClient} = useContext(BundlrClientCtx);
+
+  // image OR audio
+  const [fileType, setFileType] = useState("image");
+  const [solTxAddr, setSolTxAddr]   = useState(props?.solAddr);
+
+  const [fileSelection, setFileSelection] = useState("");
+
+  const setFile = () => {
+      var fileInputEl = document.createElement("input");
+      fileInputEl.type = "file";
+      fileInputEl.accept = `${fileType}/*`;
+      fileInputEl.style.display = "none";
+      document.body.appendChild(fileInputEl);
+      fileInputEl.addEventListener("input", function (e) {
+        setFileSelection(e.target.files[0]);
+        document.body.removeChild(fileInputEl);
+      });
+      fileInputEl.click();
+  };
+
+  const uploadFile = () => {
+    bundlrClient.uploadFile(fileSelection, fileType, solTxAddr);
+  }
 }
