0b247f68166ba24c5ae8f07d16a3ca7439e991db
0xMillionz <madokakaname99@protonmail.com>
2022-08-10 11:14:10 -0400

    removing all the things we don't need

--- apps/IDO/_context/helixContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-import {HelixClient} from "helix-clients";
-
-const HelixContext = createContext({
-	client: new HelixClient(),
-	setClient: () => {},
-});
-
-export default HelixContext;
